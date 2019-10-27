ce1e6a14f33872c1a21caa609255e3c3af172c75
Weslley da silva <weslley-dasilva@outlook.com>
2019-10-27 08:43:35 -0300

    Atualizando Vagantfile

---
+++ script.sh
@@ -0,0 +1,9 @@
+KEYPATH='/vagrant/keys'
+mkdir -p /root/.ssh
+cp $KEYPATH/key /root/.ssh/id_rsa
+cp $KEYPATH/key /root/.ssh/id_rsa.pub
+cp $KEYPATH/key.pub /root/.ssh/authorized_keys
+chmod 400 /root/.ssh/id*
+
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+
