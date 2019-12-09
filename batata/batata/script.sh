9ca6703e4257a214cda9d8aef8c9123f133553df
weslley <weslley@DeBiAn>
2019-12-09 16:01:55 -0300

    Del batata

--- batata/batata/script.sh
+++
@@ -1,9 +0,0 @@
-KEYPATH='/vagrant/keys'
-mkdir -p /root/.ssh
-cp $KEYPATH/key /root/.ssh/id_rsa
-cp $KEYPATH/key /root/.ssh/id_rsa.pub
-cp $KEYPATH/key.pub /root/.ssh/authorized_keys
-chmod 400 /root/.ssh/id*
-
-cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-
